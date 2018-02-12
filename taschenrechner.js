245cab6ddaf7f5b1589a0097a47313dc238e0bdd
damjanko3112000 <driftdriver00@gmail.com>
2018-02-12 16:14:54 +0100

    calculator

--- taschenrechner.js
+++ taschenrechner.js
@@ -1,10 +1,38 @@
 var add  = () => {
   var eingabe1 =  document.getElementById("eingabe1");  
-  var eingabe2 = document.getElementById("eingabe2")
+  var eingabe2 = document.getElementById("eingabe2");
   var e = parseInt(eingabe1.value) + parseInt(eingabe2.value);
-  var ergebnis = document.getElementById("ergebnis")
-  ergebnis.value = e
-  console.log (e)
-  result = "ergebnis"
-   result.id = "ergebnis";
+  var ergebnis = document.getElementById("ergebnis");
+  var message = document.getElementById("message");
+  message.innerText = "";
+  ergebnis.value = e;
 };
+var sub = () => {
+  var eingabe1 =  document.getElementById("eingabe1");  
+  var eingabe2 = document.getElementById("eingabe2");
+  var e = parseInt(eingabe1.value) - parseInt(eingabe2.value);
+  var ergebnis = document.getElementById("ergebnis");
+  var message = document.getElementById("message"); 
+  message.innerText = ""; 
+  ergebnis.value = e;
+}
+var multiply = () => {
+  var eingabe1 =  document.getElementById("eingabe1");  
+  var eingabe2 = document.getElementById("eingabe2");
+  var e = parseInt(eingabe1.value) * parseInt(eingabe2.value);
+  var ergebnis = document.getElementById("ergebnis");
+  var message = document.getElementById("message");
+  message.innerText = "";
+  ergebnis.value = e;
+}
+var divide = () => {
+  var eingabe1 =  document.getElementById("eingabe1");  
+  var eingabe2 = document.getElementById("eingabe2");
+  var message = document.getElementById("message");
+  if (eingabe2.value== "0"){
+    message.innerText = "Division durch null nicht erlaubt";
+  }
+  var e = parseInt(eingabe1.value) / parseInt(eingabe2.value);
+  var ergebnis = document.getElementById("ergebnis");
+  ergebnis.value = e;
+}
