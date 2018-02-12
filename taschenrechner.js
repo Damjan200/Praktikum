e93279fd261d9b4b3122f06c857179d983659bbd
damjanko3112000 <driftdriver00@gmail.com>
2018-02-12 15:40:08 +0100

    Calculator

--- taschenrechner.js
+++ taschenrechner.js
@@ -1,4 +1,10 @@
-var helloWorld = () => {
-  var result =  document.getElementById("result");  
-   result.innerHTML = "Hello World";
+var add  = () => {
+  var eingabe1 =  document.getElementById("eingabe1");  
+  var eingabe2 = document.getElementById("eingabe2")
+  var e = parseInt(eingabe1.value) + parseInt(eingabe2.value);
+  var ergebnis = document.getElementById("ergebnis")
+  ergebnis.value = e
+  console.log (e)
+  result = "ergebnis"
+   result.id = "ergebnis";
 };
