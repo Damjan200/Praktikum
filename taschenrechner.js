cd2fea06c324472b3d2d3fe71212a8ae8518e11c
damjanko3112000 <driftdriver00@gmail.com>
2018-02-13 11:21:00 +0100

    JQuerry

--- taschenrechner.js
+++ taschenrechner.js
@@ -1,38 +1,48 @@
+$(document).ready(function(){
+  $("#add").click(add);
+});
 var add  = () => {
-  var eingabe1 =  document.getElementById("eingabe1");  
-  var eingabe2 = document.getElementById("eingabe2");
-  var e = parseInt(eingabe1.value) + parseInt(eingabe2.value);
-  var ergebnis = document.getElementById("ergebnis");
-  var message = document.getElementById("message");
-  message.innerText = "";
-  ergebnis.value = e;
+  var eingabe1 = $("#eingabe1").val()
+  var eingabe2 = $("#eingabe2").val()
+  var e = parseInt(eingabe1) + parseInt(eingabe2);
+   $("#ergebnis").val(e)
+  //var message = document.getElementById("message");
+  //message.innerText = "";
 };
+$(document).ready(function(){
+  $("#sub").click(sub);
+});
 var sub = () => {
-  var eingabe1 =  document.getElementById("eingabe1");  
-  var eingabe2 = document.getElementById("eingabe2");
-  var e = parseInt(eingabe1.value) - parseInt(eingabe2.value);
-  var ergebnis = document.getElementById("ergebnis");
-  var message = document.getElementById("message"); 
-  message.innerText = ""; 
-  ergebnis.value = e;
+  var eingabe1 = $("#eingabe1").val();  
+  var eingabe2 = $("#eingabe2").val();
+  var e = parseInt(eingabe1) - parseInt(eingabe2);
+  $("#ergebnis").val(e)
+ // var message = document.getElementById("message"); 
+ // message.innerText = ""; 
+ // ergebnis.value = e;
 }
+$(document).ready(function(){
+  $("#multiply").click(multiply);
+});
 var multiply = () => {
-  var eingabe1 =  document.getElementById("eingabe1");  
-  var eingabe2 = document.getElementById("eingabe2");
-  var e = parseInt(eingabe1.value) * parseInt(eingabe2.value);
-  var ergebnis = document.getElementById("ergebnis");
-  var message = document.getElementById("message");
-  message.innerText = "";
-  ergebnis.value = e;
+  var eingabe1 = $("#eingabe1").val();  
+  var eingabe2 = $("#eingabe2").val();
+  var e = parseInt(eingabe1) * parseInt(eingabe2);
+  $("#ergebnis").val(e)
+  //var message = document.getElementById("message");
+//  message.innerText = "";
+  //ergebnis.value = e;
 }
+$(document).ready(function(){
+  $("#divide").click(divide);
+});
 var divide = () => {
-  var eingabe1 =  document.getElementById("eingabe1");  
-  var eingabe2 = document.getElementById("eingabe2");
+  var eingabe1 = $("#eingabe1").val();
+  var eingabe2 = $("#eingabe2").val();
   var message = document.getElementById("message");
-  if (eingabe2.value== "0"){
-    message.innerText = "Division durch null nicht erlaubt";
-  }
-  var e = parseInt(eingabe1.value) / parseInt(eingabe2.value);
-  var ergebnis = document.getElementById("ergebnis");
-  ergebnis.value = e;
+ // if (eingabe2.value== "0"){
+  //  message.innerText = "Division durch null nicht erlaubt";
+  //}
+  var e = parseInt(eingabe1) / parseInt(eingabe2);
+  var ergebnis = $("#ergebnis").val(e);
 }
