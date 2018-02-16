3c6527eaba0917fcb1f0c714fee06110bb21dbbf
damjanko3112000 <driftdriver00@gmail.com>
2018-02-16 15:30:01 +0100

    css plus html kürzer geschrieben

--- Button Game/buttongame.js
+++ Button Game/buttongame.js
@@ -1,12 +1,17 @@
 $(document).ready(function(){
     var  printName = (element) => {
         var KastenNr = $("#KastenNr").first().val();
+        if ( KastenNr > 9 || KastenNr < 1 ){
+         $("#message").html("Nummer zu klein/ zu gross");
+        }  
+        if (isNaN(KastenNr)) { alert('Bitte nur zahlen eingeben!') }
+        else {
         var button   = $("#button" + KastenNr);
         $(element.target).text(button.text());
         var buttonBackground = button.css("background");
-        $(element.target).css("background", buttonBackground)
+        $(element.target).css("background", buttonBackground);
         var background = button.css();
-    };
+    }};
 
     $("button").click(printName);
 });
