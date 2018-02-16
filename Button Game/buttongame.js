51b815707dbe0ff8c1cc8cd5ce07147f2753ff93
damjanko3112000 <driftdriver00@gmail.com>
2018-02-16 11:25:46 +0100

    Button Game finished

--- Button Game/buttongame.js
+++ Button Game/buttongame.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
     var  printName = (element) => {
-        //var  blue  = $(".1blue");
-        console.log($(element.target).text());
+        var KastenNr = $("#KastenNr").first().val();
+        var button   = $("#button" + KastenNr);
+        $(element.target).text(button.text());
+        var buttonBackground = button.css("background");
+        $(element.target).css("background", buttonBackground)
+        var background = button.css();
     };
 
     $("button").click(printName);
