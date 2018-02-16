92a24620d8e7cff46b4eb46b24240f092be3442e
damjanko3112000 <driftdriver00@gmail.com>
2018-02-16 11:27:48 +0100

    überarbeitubg

--- taschenrechner.js
+++ taschenrechner.js
@@ -6,8 +6,6 @@ var add  = () => {
   var eingabe2 = $("#eingabe2").val()
   var e = parseInt(eingabe1) + parseInt(eingabe2);
    $("#ergebnis").val(e)
-  //var message = document.getElementById("message");
-  //message.innerText = "";
 };
 $(document).ready(function(){
   $("#sub").click(sub);
@@ -17,9 +15,6 @@ var sub = () => {
   var eingabe2 = $("#eingabe2").val();
   var e = parseInt(eingabe1) - parseInt(eingabe2);
   $("#ergebnis").val(e)
- // var message = document.getElementById("message"); 
- // message.innerText = ""; 
- // ergebnis.value = e;
 }
 $(document).ready(function(){
   $("#multiply").click(multiply);
@@ -29,9 +24,6 @@ var multiply = () => {
   var eingabe2 = $("#eingabe2").val();
   var e = parseInt(eingabe1) * parseInt(eingabe2);
   $("#ergebnis").val(e)
-  //var message = document.getElementById("message");
-//  message.innerText = "";
-  //ergebnis.value = e;
 }
 $(document).ready(function(){
   $("#divide").click(divide);
@@ -40,9 +32,6 @@ var divide = () => {
   var eingabe1 = $("#eingabe1").val();
   var eingabe2 = $("#eingabe2").val();
   var message = document.getElementById("message");
- // if (eingabe2.value== "0"){
-  //  message.innerText = "Division durch null nicht erlaubt";
-  //}
   var e = parseInt(eingabe1) / parseInt(eingabe2);
   var ergebnis = $("#ergebnis").val(e);
 }
