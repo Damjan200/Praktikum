d41dd5dedb0fbd1ba6042eb8ab2c6c4e44f1d7ec
damjanko3112000 <driftdriver00@gmail.com>
2018-02-19 14:14:47 +0100

    changes

--- buttons/buttons.js
+++ buttons/buttons.js
@@ -4,8 +4,17 @@ function groß(){
 $(document).ready(function(){
     $(".groß").click(groß);
     $(".mittel").click(mittel);
-    $(".klein").click(klein)
-    $("button").click(button)
+    $(".klein").click(klein);
+    $("button").click(button);
+    $("#g1").click(g1);
+    $("#g2").click(g2);
+    $("#g3").click(g3);
+    $("#m1").click(m1);
+    $("#m2").click(m2);
+    $("#m3").click(m3);
+    $("#k1").click(k1);
+    $("#k2").click(k2);
+    $("#k3").click(k3);
 });
 function mittel(){
     console.log("mittel");
@@ -14,6 +23,31 @@ function klein(){
     console.log("klein");
 }
 function button(){
-    console.log("button")
+    console.log("button");
 }
-
+function  g1(){
+    console.log("#g1");
+}
+function g2(){
+    console.log ("#g2")
+}
+function  g3(){
+    console.log("#g3");
+}
+function  m1(){
+    console.log("#m1");
+}function  m2(){
+    console.log("#m2");
+}
+function  m3(){
+    console.log("#m3");
+}
+function  k1(){
+    console.log("#k1");
+}
+function k2(){
+    console.log("#k2");
+}
+function  k3(){
+    console.log("#k3");
+}
