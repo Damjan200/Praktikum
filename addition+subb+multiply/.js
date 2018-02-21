c1c6b84c452b78a92a9226d23704b8be3d6addfc
damjanko3112000 <driftdriver00@gmail.com>
2018-02-21 15:33:10 +0100

    changes

--- addition+subb+multiply/.js
+++ addition+subb+multiply/.js
@@ -3,6 +3,13 @@ $(document).ready(function(){
     $("#calc2").click(calc2);
     $("#calc3").click(calc3);
     $("#calc4").click(calc4);
+    var numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20] 
+    //var Farben = ["blau", "grün", "rot","gelb","orange"]
+   // for (Farben=0;Farben<6;Farben=Farben+1)
+    //console.log(Farben)
+    for (i=0;i<21;i=i+1){
+    console.log(numbers[i])
+}
 })
 function minus(a,b){
  var m = a-b;
